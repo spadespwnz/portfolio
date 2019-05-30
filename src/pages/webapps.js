e1158e3b9a3f41d1102d710b0df242844fe9c4d1
Nathan Brown <spades@live.no>
2019-05-30 01:52:28 -0400

    Finished webtools page

--- src/pages/webapps.js
+++ src/pages/webapps.js
@@ -56,15 +56,40 @@ export default ({data}) => (
             </p>
             <p>
                 <b css={{display:`block`, opacity:`.5`}}>Notes:</b> 
-                
+                The notes page is a simple WebApp giving users the ability to create and write notes. These notes are automatically stored on the server, and regularly saved 
+                to the server as they are updated. They can only be accessed by the user that created them.
             </p>
             <p>
                 <b css={{display:`block`, opacity:`.5`}}>Image Uploading and Sharing:</b> 
-                
+                <div css={{width:`100%`, textAlign:`center`, marginTop:`10px`, marginBottom:`10px`}}>
+                    <video css={{maxWidth:`80%`, marginLeft:`10%`, marginRight:`10%`}} src="/vid/webtools/upload.webm" muted autoPlay="autoplay" loop></video>
+                    <span css={{fontSize:`.75em`, opacity:`.5`}}>Take a screenshot, paste it in, crop, upload, and share!</span>
+                </div>
+                The image uploading tool is designed to make it quick and easy to share pictures. Simply paste a picture into the window, and it will be added to the canvas. 
+                From there the image can be modified by cropping, rotation, or drawing boxes. Once editing is finished the image can be uploaded with a press of a button, and will instantly 
+                allow you to copy the URL of the uploaded image. The uploading tool will also make use of WebStorage if the upload fails, and allow you to upload the images from storage 
+                once you connected. When these images are uploaded they are added to the user's gallery, which can be accessed by anyone.
+            </p>
+            <p>
+                These images are stored on Amazon's S3 storage service, which allows exceedingly fast load times.
             </p>
             <p>
                 <b css={{display:`block`, opacity:`.5`}}>Friends and Chat:</b> 
-                
+                <ImageText fluid={data.friends1.childImageSharp.fluid}  mirror={true}>
+                    The friends system works by sending friend requests to other users. The other user can then accept or decline the request.
+                </ImageText>
+                Once users have become friends they will see each other in the friends list, as well as see an indicator showing if they are currently online. 
+                From the friends list, users can send messages which will 
+                be shown to the other users as a flashing window that can be maximized. Using this window the users can chat in real time. The chat window can be minimized, and will begin 
+                flashing again if there are any new unread messages.
+            </p>
+            <div css={{width:`100%`, textAlign:`center`, marginTop:`10px`, marginBottom:`10px`}}>
+                <video css={{maxWidth:`80%`, marginLeft:`10%`, marginRight:`10%`}} src="/vid/webtools/chat2.webm" muted autoPlay="autoplay" loop></video>
+            </div>
+            <p>
+                This chatting system is powered through WebSockets. Along with the chat windows and friends list that is accessible on all authenticated routes, there is a friends page 
+                which allows viewing of all messages sent between the 2 users. These messages are stored in a mongoDB database as they as sent through the WebSockets. Chatting is also 
+                possible on this friends page using the conversation history window.
             </p>
         </Project>
     </PageTemplate>
@@ -81,6 +106,15 @@ export const query = graphql`
       }
     },
     img1: file(relativePath: { eq: "webapps/img1.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      },
+      friends1: file(relativePath: { eq: "webapps/friends1.png" }) {
         childImageSharp {
           # Specify the image processing specifications right in the query.
           # Makes it trivial to update as your page's design changes.
